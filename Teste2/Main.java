fd474a5461a0e50f1c6be0206be627896e1ee291
MatheusZanfolin <matzanfolin@gmail.com>
2018-11-12 11:00:14 -0200

    Peer 2 Aprimorado

--- Teste2/Main.java
+++ Teste2/Main.java
@@ -5,37 +5,41 @@ public class Main{
 	private static BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args){
 		meuControlador = new Controlador();
-		System.out.println("Começando a tratar broadcasting..");
+		//System.out.println("Começando a tratar broadcasting..");
 		meuControlador.inicializarBroadcasting();
-		System.out.println("Enviando broadcasting..");
+		System.out.println("Peer 2: Enviando requisição...");
 		meuControlador.enviar();
 	}
 	public static void depoisEnviar(){
-		System.out.println("Enviado!!");
+		//System.out.println("Enviado!!");
 		//meuControlador.finalizarBroadcasting();
-		System.out.println("Escutando resposta!");
+		System.out.println("Peer 2: Aguardando resposta...");
 		meuControlador.inicializarEscuta();
-		System.out.println("Escutando....");
+		//System.out.println("Escutando....");
 		meuControlador.receber();
 	}
 	public static void depoisEscuta(){
 		Peer achado = meuControlador.getPeerAchado();
-		System.out.println(achado.getIP());
-		System.out.println("Recebido!!");
 
-		try{
+		System.out.println("Peer 2: Resposta recebida!");
+		System.out.println("Peer 2: Requisição recebida pelo peer de IP " + achado.getIP().getHostAddress());
+
+		try {
+			System.out.print("Mensagem: ");
 
 			String x = teclado.readLine();
+
+			System.out.println();
 		}
 		catch(Exception e){
 			System.out.println(e.getMessage());
 		}
 		meuControlador.inicializarTCP();
-		System.out.println("Enviando TCP!!");
+		//System.out.println("Enviando TCP!!");
 		meuControlador.enviarTCP();
 
 	}
 	public static void depoisEnviarTCP(){
-		System.out.println("Enviado TCP!!");
+		System.out.println("Mensagem enviada!");
 	}
 }
